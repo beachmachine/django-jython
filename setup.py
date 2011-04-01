dd60c70f15b923aaa8f36a1f8b97ea5f1b0894fa
juneau001 <juneau001@gmail.com>
2011-04-01 12:04:12 -0500

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name = "django-jython",
-    version = "1.1.2-alpha",
+    version = "1.3.0b1",
     packages = ['doj',
                 'doj.backends',
                 'doj.backends.zxjdbc',
@@ -21,14 +21,14 @@ setup(
                                      'war_skel/WEB-INF/lib/*',
                                      'war_skel/WEB-INF/lib-python/README']},
     # metadata for upload to PyPI
-    author = "Leonardo Soto M.",
-    author_email = "leo.soto@gmail.com",
+    author = "Josh Juneau",
+    author_email = "juneau001@gmail.com",
     description = "Database backends and management commands, for development under Django/Jython",
     license = "BSD",
     keywords = "django jython database java",
     url = "http://code.google.com/p/django-jython/",
     classifiers = [
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Framework :: Django",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
