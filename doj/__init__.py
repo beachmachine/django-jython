2b2d7f1bc364df098d4d5727c70c9ecff79d63f3
Andreas Stocker <andreas@st0cker.at>
2014-07-27 12:01:09 +0200

    Created branch for 1.7.x rewrite

--- doj/__init__.py
+++ doj/__init__.py
@@ -1,4 +1,11 @@
-# loading monkey patches
-from doj.monkey import *
+# -*- coding: utf-8 -*-
 
-VERSION = (1, 5, 0, 'alpha', 1)
+__VERSION = (1, 7, 0, 'alpha', 1)
+
+
+def get_version():
+    """
+    Gets the version of the library
+    :return:Version
+    """
+    return tuple(__VERSION)
