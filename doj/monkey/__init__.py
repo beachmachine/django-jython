48fd4c2daff45ec119727a54445e000acd038e2b
Andreas Stocker <andreas@st0cker.at>
2014-08-04 09:20:42 +0200

    Implement inspect.getcallargs if not existing

--- doj/monkey/__init__.py
+++ doj/monkey/__init__.py
@@ -2,8 +2,10 @@
 
 import doj.monkey.django_utils_functional_lazy
 import doj.monkey.django_http_response_streaminghttpresponse
+import doj.monkey.inspect_getcallargs
 
 
 def install_monkey_patches():
     doj.monkey.django_utils_functional_lazy.install()
     doj.monkey.django_http_response_streaminghttpresponse.install()
+    doj.monkey.inspect_getcallargs.install()
